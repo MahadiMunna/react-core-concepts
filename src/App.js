4bf094ee28e0b24482960b95419469d942a6d14a
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-20 15:30:09 +0600

    create new function and add them

--- src/App.js
+++ src/App.js
@@ -12,39 +12,32 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        {/* html and css in react */}
-
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello</h3>
-        </div>
-        <div className='music'>
-          <p style={{
-            color: "purple",
-            backgroundColor: "white"
-          }}>{222 + number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-        </div>
-
-
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <h3>New Component Friend</h3>
+      <Friend></Friend>
     </div>
   );
+}
+
+// similar in look, different in data
+
+function Person() {
+  return (
+    <div className='person'>
+      <h1>Naruto Uzumaki</h1>
+      <p>Special: rasengen, Shadow clone</p>
+    </div>
+  )
+}
+
+function Friend(){
+  return(
+    <div className='friend'>
+      <h3>Name: Sasuke Uchiha</h3>
+      <p>Special: Chidori</p>
+    </div>
+  )
 }
 
 export default App;
